90840c363eb8aae32c5773e12ab3dbd2337bbd35
Navale Pragati <101563326+PragatiNavale1@users.noreply.github.com>
2024-05-14 22:09:43 +0530

    Adjacency Matrix.cpp

--- Adjacency Matrix.cpp
+++ Adjacency Matrix.cpp
@@ -1,63 +1,102 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
 class AdjacencyMatrix
 {
-    public:
-    int vertex,edge,fuel,a[30][30];
+public:
+    int vertex, edge, fuel, a[30][30];
     string city[30];
     void create();
     void display();
+    int getFuel(string fromCity, string toCity);
 };
+
 void AdjacencyMatrix::create()
 {
-    cout<<"Enter Number of Cities : ";
-    cin>>vertex;
-    for(int i=0;i<vertex;i++)
+    cout << "Enter Number of Cities: ";
+    cin >> vertex;
+    for (int i = 0; i < vertex; i++)
     {
-        cout<<"Enter Name of City : ";
-        cin>>city[i];
+        cout << "Enter Name of City " << i + 1 << ": ";
+        cin >> city[i];
     }
-    for(int i=0;i<vertex;i++)
+    for (int i = 0; i < vertex; i++)
     {
-        for(int j=0;j<vertex;j++)
+        for (int j = 0; j < vertex; j++) 
         {
-            if(i==j)
+            if (i != j) 
             {
-                a[i][j]=0;
+                if (i < j)
+                {
+                    cout << "Enter the fuel " << city[i] << " to " << city[j] << ": ";
+                    cin >> fuel;
+                    a[i][j] = fuel;
+                    a[j][i] = fuel; // Assign the same value for symmetry
+                }
             }
             else
             {
-                    cout<<"Enter the fuel "<<city[i]<<" to "<<city[j]<<" : ";
-                    cin>>fuel;
-                    a[i][j]=fuel;
-                    a[j][i]=a[i][j];
+                a[i][j] = 0;
             }
         }
     }
 }
+
 void AdjacencyMatrix::display()
 {
-    cout<<"***Adjecancy Matrix***\n";
-    for(int i=0;i<vertex;i++)
+    cout << "***Adjacency Matrix***\n";
+    cout << "\t";
+    for (int i = 0; i < vertex; i++)
     {
-        cout<<"\t"<<city[i]<<"\t";
+        cout << city[i] << "\t";
     }
-    cout<<"\n";
-    for(int i=0;i<vertex;i++)
+    cout << "\n";
+    for (int i = 0; i < vertex; i++)
     {
-        cout<<city[i];
-        for(int j=0;j<vertex;j++)
+        cout << city[i] << "\t";
+        for (int j = 0; j < vertex; j++)
         {
-            cout<<"\t"<<a[i][j]<<"\t";
+            cout << a[i][j] << "\t";
         }
-        cout<<"\n";
+        cout << "\n";
     }
 }
+
+int AdjacencyMatrix::getFuel(string fromCity, string toCity)
+{
+    int fromIndex = -1, toIndex = -1;
+    for (int i = 0; i < vertex; i++)
+    {
+        if (city[i]== fromCity)
+            fromIndex = i;
+        if (city[i]== toCity)
+            toIndex = i;
+    }
+    if (fromIndex != -1 && toIndex != -1)
+        return a[fromIndex][toIndex];
+    else
+        return -1; // Invalid city names
+}
+
+
 int main()
 {
     AdjacencyMatrix AM;
     AM.create();
     AM.display();
+    
+    string fromCity, toCity;
+    cout << "Enter the source city: ";
+    cin >> fromCity;
+    cout << "Enter the destination city: ";
+    cin >> toCity;
+
+    int fuelRequired = AM.getFuel(fromCity, toCity);
+    if (fuelRequired != -1)
+        cout << "Fuel required from " << fromCity << " to " << toCity << ": " << fuelRequired << endl;
+    else
+        cout << "Invalid city names!" << endl;
+
     return 0;
 }
